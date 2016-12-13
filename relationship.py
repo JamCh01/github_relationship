0b82303809171ada114bb16277aa556e242ebf8a
HolaJam <jamcplusplus@gmail.com>
2016-12-13 23:01:49 +0800

    符合pep8，删除了没有用到的库

--- relationship.py
+++ relationship.py
@@ -1,5 +1,5 @@
 import api
-from py2neo import Graph, Node, Relationship, Subgraph
+from py2neo import Graph, Node, Relationship
 import gevent
 from gevent import monkey
 monkey.patch_socket()
@@ -13,12 +13,15 @@ main_node = Node('user', name=username, level=level, referer='')
 graph.create(main_node)
 
 res = {}
+
+
 def info(username):
     res['user_info'] = new.user_info(username=username)
 
 
 def followers(username):
     res['followers'] = new.user_followers(username=username)
+
 
 def following(username):
     res['following'] = new.user_following(username=username)
@@ -32,23 +35,27 @@ def repos(username):
     res['repos'] = new.user_repos(username=username)
 
 
-
-
 def user_info(username):
     gevent.joinall([
         gevent.spawn(followers, username),
         gevent.spawn(following, username),
     ])
-    each = set(res['followers']) & set(res['following']) # 互相关注为关注者和粉丝的交集
-    res['followers'] = list(set(res['followers']) - each) # 根据关注和粉丝与互相关注的差集去掉重复数据
-    res['following'] = list(set(res['following']) - each )
+    each = set(res['followers']) & set(res['following'])  # 互相关注为关注者和粉丝的交集
+    res['followers'] = list(set(res['followers']) -
+                            each)  # 根据关注和粉丝与互相关注的差集去掉重复数据
+    res['following'] = list(set(res['following']) - each)
     res['each'] = list(each)
     return res
 
+
 def find_node(level):
     # 获得同等级所有用户节点，遍历之
-    user_list = graph.find(label='user', property_key='level', property_value=level)
+    user_list = graph.find(
+        label='user',
+        property_key='level',
+        property_value=level)
     return user_list
+
 
 def find_rel(main, target):
     run = '''Match(main:user {name:"%s"})
@@ -56,10 +63,15 @@ def find_rel(main, target):
     return (main)<-[*]->(target)''' % (main, target)
     a = graph.run(run).evaluate()
     return a
+
+
 def draw(res, referer, level):
     # 在neo4j中绘制节点和关系
     # main_node为name: referer
-    main_node = graph.find_one('user', property_key='name', property_value=referer)
+    main_node = graph.find_one(
+        'user',
+        property_key='name',
+        property_value=referer)
     for key in res.keys():
         for i in res[key]:
             # 暂存节点，label:user name:表示referer的关系用户，包括follower，following，each
@@ -71,9 +83,9 @@ def draw(res, referer, level):
                 continue
             rel = Relationship(main_node, key, tmp)
             graph.create(rel)
-while level !=5:
+while level != 5:
     for i in find_node(level=level):
         print(i['name'])
         res = user_info(username=i['name'])
-        draw(res = res, referer=i['name'], level=level+1)
+        draw(res=res, referer=i['name'], level=level + 1)
     level += 1
