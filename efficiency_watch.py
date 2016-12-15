c599d5065f879dcedc1db3923c1326498c67edcd
HolaJam <jamcplusplus@gmail.com>
2016-12-15 19:17:14 +0800

    修改效率监控功能，可以编辑crontab来实现定时，输出在log.txt中

--- efficiency_watch.py
+++ efficiency_watch.py
@@ -1,4 +1,10 @@
-from py2neo import Node, Graph, Relationship
+import datetime
+from py2neo import Graph
 
 graph = Graph(password='test')
-tmp = graph.run('''MATCH (n) RETURN count(n)''').data()
+def eff():
+    tmp = graph.run('''MATCH (n) RETURN count(n)''').evaluate()
+    return tmp
+
+with open('log.txt','a') as f:
+    f.write('%s %s' % (str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")), eff()))
