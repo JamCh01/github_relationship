13b20713515a9246463b7002bb63745843c4b869
HolaJam <jamcplusplus@gmail.com>
2016-12-19 17:49:52 +0800

    fix some bug

--- relationship_mariadb.py
+++ relationship_mariadb.py
@@ -4,6 +4,7 @@ from common_spider import user_info
 user_name = 'HolaJam'
 level = 0
 
+
 def mariadb_connection():
     # 连接MariaDB
     conn = mariadb.connect(
@@ -15,16 +16,26 @@ def mariadb_connection():
         charset='UTF8')
     return conn
 
+
 def mariadb_insert(user_name, level, referer, type):
     with mariadb_connection() as cursor:
         SQL = '''INSERT INTO relationship (user_name, level, referer, type) VALUES (%s,%s,%s,%s)'''
         cursor.execute(SQL, (user_name, level, referer, type))
 
-def mariadb_select(user_name, level, referer, type):
+
+def mariadb_select_forward(user_name, referer):
     with mariadb_connection() as cursor:
-        SQL = '''SELECT * FROM relationship WHERE user_name=%s AND referer=%s AND level=%s AND type=%s'''
-        cursor.execute(SQL, (user_name, referer, level + 1, type))
+        SQL = '''SELECT * FROM relationship WHERE user_name=%s AND referer=%s'''
+        cursor.execute(SQL, (user_name, referer))
     return cursor.fetchone()
+
+
+def mariadb_select_reverse(user_name, referer):
+    with mariadb_connection() as cursor:
+        SQL = '''SELECT * FROM relationship WHERE user_name=%s AND referer=%s'''
+        cursor.execute(SQL, (referer, user_name))
+    return cursor.fetchone()
+
 
 def find_all_level(level):
     with mariadb_connection() as cursor:
@@ -40,11 +51,12 @@ def draw(res, referer, level):
         for i in res[key]:
             # 校验是否存在本关系
             # 不存在时候插入
-            if mariadb_select(
+            if mariadb_select_forward(
+                user_name=i,
+                referer=referer,
+            ) is None and mariadb_select_reverse(
                     user_name=i,
-                    level=level,
-                    referer=referer,
-                    type=key) is None:
+                    referer=referer) is None:
                 # 判断关系是否为each
                 if key == 'each':
                     mariadb_insert(
@@ -74,9 +86,9 @@ def draw(res, referer, level):
                         referer=referer,
                         type=key)
 
-mariadb_insert(user_name=user_name,level=level, referer=' ', type='self')
+mariadb_insert(user_name=user_name, level=level, referer=' ', type='self')
 while level != 5:
     for i in find_all_level(level=level):
-        print(i)
+        # print(i)
         draw(res=user_info(username=i[1]), referer=i[1], level=level)
     level += 1
