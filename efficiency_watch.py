6e58a17bc7f69fbfbfc37af56766872f3aec537d
HolaJam <jamcplusplus@gmail.com>
2016-12-24 21:34:59 +0800

    修正更新mariadb效率检测

--- efficiency_watch.py
+++ efficiency_watch.py
@@ -1,14 +1,33 @@
 import time
+import sys
 import datetime
 from py2neo import Graph
+import pymysql as mariadb
 
-graph = Graph(password='test')
-def eff():
+
+def neo4j():
+    graph = Graph(password='test')
     tmp = graph.run('''MATCH (n) RETURN count(n)''').evaluate()
-    with open('log.txt', 'a') as f:
+    with open('eff.txt', 'a') as f:
+        f.write('%s %s' % (str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")), tmp))
+def maria():
+    conn = mariadb.connect(
+        host='localhost',
+        port=3306,
+        user='root',
+        passwd='test',
+        db='github',
+        charset='UTF8')
+    with conn as cursor:
+        SQL = '''SELECT COUNT(*) FROM relationship'''
+        cursor.execute(SQL)
+    tmp = cursor.fetchone()
+    cursor.close()
+    with open('eff.txt', 'a') as f:
         f.write('%s %s' % (str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")), tmp))
 
 
-while True:
-    eff()
-    time.sleep(3600)
+if sys.argv[1] == 'neo4j':
+    neo4j()
+elif sys.argv[0] == 'maria':
+    maria()
